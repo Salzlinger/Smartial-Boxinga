84d31e4197904a27f35c5ebe1741ea45b453a54b
Salzlinger <73530579+Salzlinger@users.noreply.github.com>
2021-01-13 12:42:08 +0100

    Create EventManager.cs

---
+++ code/Smartial Boxinga/Assets/EventManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+  /*  public delegate void hitManager();
+    public static event hitManager onHit;
+
+    public void hit()
+    {
+        if (onHit != null)
+        {
+            onHit();
+        }
+    } */
+}
