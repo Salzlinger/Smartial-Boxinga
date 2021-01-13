3dc8ac081d1303a7d09e3cf2ac859c287a717e4a
Salzlinger <73530579+Salzlinger@users.noreply.github.com>
2021-01-13 12:42:13 +0100

    Create FirstStage.cs

---
+++ code/Smartial Boxinga/Assets/FirstStage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirstStage : MonoBehaviour
+{
+    public GameObject Toll;
+
+    public void StartGame()
+    {
+        Destroy(GameObject.Find("Dialog1"));
+        Instantiate(Toll, new Vector3(0, 2.65f, -5.27f), Quaternion.identity);
+    }
+}
