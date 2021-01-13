d2cbb2667735cd435e97296fd26da478e89fdda9
Salzlinger <73530579+Salzlinger@users.noreply.github.com>
2021-01-13 12:42:24 +0100

    Create jabLTrigger.cs

---
+++ code/Smartial Boxinga/Assets/jabLTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jabLTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // checkt ob der schlag in der Combo grade auch auszuführen ist.
+        // man kann hier noch eine Fehler respons mit einem else statement hinzufügen.
+        if (comboChecker.comboList[0] == "jab")
+        {
+            //EventManager.hit();
+            Destroy(gameObject);
+            Debug.Log("Destroyed!!!");
+        }
+    }
+ 
+        // Start is called before the first frame update
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
