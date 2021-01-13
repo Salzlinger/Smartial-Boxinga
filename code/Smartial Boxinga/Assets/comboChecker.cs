29df2f667755b04a33c8545da9cdc8d4da3a7fe9
Salzlinger <73530579+Salzlinger@users.noreply.github.com>
2021-01-13 12:42:02 +0100

    Create comboChecker.cs

---
+++ code/Smartial Boxinga/Assets/comboChecker.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class comboChecker : MonoBehaviour
+{
+    // Array welches mit der auszuführenden Combo befüllt wird
+    int n = 3;
+    public static string[] comboList = new string [3] { "jab", "cross", "hook" };
+    int i = 0;
+    int pos = 0;
+    //public string currentPunch = comboList[0];
+
+
+    void OnEnable()
+    {
+       // EventManager.onHit += bullseye;
+    }
+
+    void OnDisable()
+    {
+       // EventManager.onHit -= bullseye;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // entfernt das getroffene Ziel aus dem Array
+    void bullseye ()
+    {
+        for (i = 0; i < 3; i++)
+        {
+            comboList[i] = comboList[i + 1];
+        }
+        //currentPunch = comboList[0];
+    }
+}
