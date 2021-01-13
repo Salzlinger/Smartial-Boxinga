574b1cc62694e57874ac97a3bdd69104e325bef2
Salzlinger <73530579+Salzlinger@users.noreply.github.com>
2021-01-13 12:51:22 +0100

    einfügen der projekt datei
    keine VR umgebung

---
+++ code/Smartial Boxinga/Assets/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Quest quest;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ code/Smartial Boxinga/Assets/questGiver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class questGiver : MonoBehaviour
+{
+    public Quest quest;
+
+    public Player player;
+
+    public GameObject questWindow;
+    public Text titleText;
+    public Text descriptionText;
+
+    public void OpenQuestWindow()
+    {
+        questWindow.SetActive(true);
+        titleText.text = quest.title;
+        descriptionText.text = quest.description;
+    }
+
+    public void AcceptQuest()
+    {
+        questWindow.SetActive(false);
+        quest.isActive = true;
+        player.quest = quest;
+    }
+}
